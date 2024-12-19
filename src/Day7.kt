ed991f53a600095dc3e62e52b1505e1c2749c81c
Sankara Rameswaran <sankara.rameswaran@jpmorgan.com>
2024-12-19 15:14:10 -0800

    feat(day-7): Advent of code Day 7. Equations - bitwise - part 1

---
+++ src/Day7.kt
@@ -0,0 +1,34 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+import kotlin.math.pow
+
+fun main() {
+    val opers: List<Pair<String, (Long, Long) -> Long>> = listOf(
+        "+" to { a, b -> a + b },
+        "*" to { a, b -> a * b })
+    val sum = readInputs().filter { (res, nums) -> checkCombinations(res, nums, opers) }.sumOf { it.first }
+    println("Sum = $sum")
+}
+
+private fun readInputs(): List<Pair<Long, List<Long>>> {
+    return Path("resources/day-7.input.txt").readLines().map { line ->
+        val (head, tail) = line.split(":")
+        val res = head.trim().toLong()
+        val nums = tail.trim().split(" ").map(String::trim).map(String::toLong)
+        res to nums
+    }
+}
+
+private fun checkCombinations(res: Long, values: List<Long>, opers: List<Pair<String, (Long, Long) -> Long>>): Boolean {
+    println("res: $res, values: $values")
+    val numOps = opers.size.toDouble()
+    for (i in 0..<numOps.pow(values.size - 1).toInt()) {
+        var sum = values[0]
+        for (j in values.size - 2 downTo 0) {
+            val oper = (i shr j) and 1
+            sum = opers[oper].second(sum, values[values.size - j - 1])
+        }
+        if (sum == res) return true
+    }
+    return false
+}
